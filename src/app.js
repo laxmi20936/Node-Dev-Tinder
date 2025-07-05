02e6c9a6d5bd38b3d547e85757797f654f2b3750
Laxmi <waghmorelaxmi@gmail.com>
2025-07-05 19:31:56 +0530

    added document to db

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require("express");
-require("./config/database")
+const { connectDB } = require("./config/database");
+const User = require("./models/user");
+
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send("Connected");
+app.post("/signup", async (req, res) => {
+  // Creating a new instance of the User model
+  const user = new User({
+    firstName: "Laxmi",
+    lastName: "Waghmore",
+    emailId: "xyz@gmail.com",
+    password: "laxmi@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
 });
 
-app.listen(7777, () => {
-  console.log("Listening to port 7777");
-});
+connectDB()
+  .then(() => {
+    console.log("Connected to db");
+    app.listen(7777, () => {
+      console.log("Listening to port 7777");
+    });
+  })
+  .catch((err) => console.err("DB cannot be connected"));
--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://waghmorelaxmi:EYEW9MHM5bHImEW6@cluster0.qx2wqxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://waghmorelaxmi:EYEW9MHM5bHImEW6@cluster0.qx2wqxa.mongodb.net/devTinderBackend";
 
 const connectDB = async () => {
   await mongoose.connect(uri);
 };
 
 // this will first listen to the port and connect to DB which is incorrect
-connectDB()
-  .then(() => console.log("Connected to db"))
-  .catch((err) => console.err("DB cannot be connected"));
+// connectDB()
+//   .then(() => console.log("Connected to db"))
+//   .catch((err) => console.err("DB cannot be connected"));
+
+module.exports = { connectDB };
