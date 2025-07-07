8b39722b236cf824ff1eb9c5475a109bbd2ff17c
Laxmi <waghmorelaxmi@gmail.com>
2025-07-07 15:25:25 +0530

    post, get, getById

--- src/app.js
+++ src/app.js
@@ -3,21 +3,61 @@ const { connectDB } = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  // Creating a new instance of the User model
-  const user = new User({
-    firstName: "Laxmi",
-    lastName: "Waghmore",
-    emailId: "xyz@gmail.com",
-    password: "laxmi@123",
-  });
+  //   console.log(req);
+  //   Creating a new instance of the User model
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: "jsgh@gmail.com" });
+    if (users.length === 0) {
+      res.send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmailId = req.body.email;
+  try {
+    const user = await User.findOne({ emailId: userEmailId });
+    if (!user) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/userByID", async (req, res) => {
+  try {
+    const id = req.body.id;
+    const userByID = await User.findById({ _id: id });
+    console.log(userByID);
+    // const userByID = await User.findById(id);
+    if (!userByID) {
+      res.send("User not Found");
+    } else {
+      res.send(userByID);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
@@ -28,4 +68,4 @@ connectDB()
       console.log("Listening to port 7777");
     });
   })
-  .catch((err) => console.err("DB cannot be connected"));
+  .catch((err) => console.error("DB cannot be connected"));
