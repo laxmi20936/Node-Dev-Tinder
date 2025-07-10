9771640d131ff7ffafbcc73f72b4c031fd6e8e62
Laxmi <waghmorelaxmi@gmail.com>
2025-07-10 18:12:07 +0530

    validation  correction

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -7,11 +7,15 @@ const validate = (data) => {
     throw new Error("Use a strong Password");
   } else if (!validator.isEmail(emailId)) {
     throw new Error("Name is not valid");
-  } else if (age && age < 18) {
-    throw new Error("Agee should be greater than 18");
-  } else if (skills?.length > 10) {
-    throw new Error("Up can add only 10 skills");
-  }
+  } 
+  
+//   else if (age && age < 18) {
+//     throw new Error("Agee should be greater than 18");
+//   } else if (skills?.length > 10) {
+//     throw new Error("Up can add only 10 skills");
+//   } 
+//     This code is not needed while signup
+//     Sign up not needs firstName, LastName, email, password
 };
 
 module.exports = { validate };
